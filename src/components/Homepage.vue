2d417a79e88846223018f8bad3d229635cda22ef
Jack Quigley <jackaquigley@gmail.com>
2020-07-23 19:39:56 +0100

    added home page basic elements

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,6 +1,16 @@
 <template>
-  <div id="">
-    <p>This is rendered by the Home component.</p>
+  <div id="homepageWrapper">
+    <p>Welcome to Pollokshields!</p>
+    <p>The original, the biggest and best-preserved planned garden suburb in the UK.</p>
+    <p>A Conservation area</p>
+    <img src="https://pollokshieldsheritage.co.uk/img/PH%20web%20home%20image.jpg" alt="Images Of Pollokshields">
+    <div>
+      <p>AGM Reports</p>
+      <p>Latest Planning Applications</p>
+      <p>Lecture Series And AGM
+        <span id="redTextHighlight">Postponed</span> </p>
+      <p>Guided Walk Spring 2020 <span id="redTextHighlight">Cancelled</span> </p>
+    </div>
   </div>
 </template>
 <script>
@@ -8,5 +18,18 @@ export default {
   name: "home",
 }
 </script>
-<style lang="scss" scoped>
+<style scoped>
+
+#homepageWrapper {
+  text-align: center;
+}
+
+#redTextHighlight {
+  color: red;
+}
+
+img {
+
+}
+
 </style>
