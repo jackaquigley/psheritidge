a28e969df6952273c15034adf2afda6e56154185
Jack Quigley <jackaquigley@gmail.com>
2020-07-23 19:31:20 +0100

    added links to the header component, made empty links for the ones we've not sectioned out

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,31 @@
 <template>
   <div id="headerWrapper">
-    <router-link :to="{ name: 'homepage'}">Home</router-link>
-    <router-link :to="{ name: 'history'}">History</router-link>
+    <header>
+      <div id="titleWrapper">
+        <h1>Pollokshields Heritage</h1>
+      </div>
+      <div id="donateWrapper">
+        <h1>Donate.</h1>
+      </div>
+      <div id="linksWrapper">
+        <router-link :to="{ name: 'homepage'}">Home</router-link>
+        <a>About Us</a>
+        <a>Contact Us</a>
+        <a>News</a>
+        <a>Location</a>
+        <a>Home</a>
+        <router-link :to="{ name: 'history'}">History</router-link>
+        <a>What Others Say</a>
+        <a>Conservation Advice</a>
+        <a>Publications</a>
+        <a>Design Matters</a>
+        <a>Useful Links</a>
+        <a>Events</a>
+        <a>How To Join</a>
+        <a>Projects</a>
+        <a>Gallary</a>
+      </div>
+    </header>
   </div>
 </template>
 
@@ -13,8 +37,30 @@ export default {
 </script>
 <style scoped>
 
-router-link {
-  background: blue;
+a {
+  padding: 2px;
+  border: 1px solid black;
 }
+
+header {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+#titleWrapper {
+  width: 70%;
+  text-align: center;
+}
+
+#donateWrapper {
+  width: 30%;
+  text-align: center;
+}
+
+#linksWrapper {
+  align-self: center;
+  }
 
 </style>
