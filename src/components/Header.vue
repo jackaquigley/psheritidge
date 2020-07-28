7c0fa95f78a20aa0df39eb035033222a8dc37ce8
Jack Quigley <jackaquigley@gmail.com>
2020-07-28 17:21:11 +0100

    finished homepagestyling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,23 +11,25 @@
         </div>
       </div>
       <div id="linksWrapper">
-        <router-link :to="{ name: 'homepage'}">Home</router-link>
-        <a>About Us</a>
-        <a>Contact Us</a>
-        <a>News</a>
-        <a>Location</a>
-        <a>Home</a>
-        <router-link :to="{ name: 'history'}">History</router-link>
-        <a>What Others Say</a>
-        <a>Conservation Advice</a>
-        <a>Publications</a>
-        <a>Design Matters</a>
-        <a>Useful Links</a>
-        <a>Events</a>
-        <a>How To Join</a>
-        <a>Projects</a>
-        <a>Gallery</a>
-      </div>
+        <div id="buttonsWrapper">
+        <router-link class="buttonLink" :to="{ name: 'homepage'}">Home</router-link>
+          <a class="buttonLink">About Us</a>
+          <a class="buttonLink">Contact Us</a>
+          <a class="buttonLink">News</a>
+          <a class="buttonLink">Location</a>
+          <router-link class="buttonLink" :to="{ name: 'donate'}">Donate</router-link>
+          <router-link class="buttonLink" :to="{ name: 'history'}">History</router-link>
+          <a class="buttonLink">What Others Say</a>
+          <a class="buttonLink">Conservation Advice</a>
+          <a class="buttonLink">Publications</a>
+          <a class="buttonLink">Design Matters</a>
+          <a class="buttonLink">Useful Links</a>
+          <a class="buttonLink">Events</a>
+          <a class="buttonLink">How To Join</a>
+          <a class="buttonLink">Projects</a>
+          <a class="buttonLink">Gallery</a>
+        </div>
+    </div>
     </header>
   </div>
 </template>
@@ -38,11 +40,21 @@ export default {
 }
 
 </script>
+
 <style scoped>
 
 a {
-  padding-left: 5px;
-  padding-right: 5px;
+  padding-left: 30px;
+  padding-right: 30px;
+  font-family: Garamond;
+  font-size: 20px;
+  height: 20px;
+  text-decoration: none;
+  color: black;
+}
+
+h1 {
+  font-family: georgia;
 }
 
 header {
@@ -50,6 +62,15 @@ header {
   flex-direction: row;
   justify-content: center;
   flex-wrap: wrap;
+}
+
+#buttonsWrapper {
+  background: lightblue;
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: space-around;
+  flex-flow: row wrap;
 }
 
 #titleWrapper {
@@ -63,18 +84,29 @@ header {
   align-items: center;
 }
 
-#innerDonate {
-
-}
 
 #linksWrapper {
   align-self: center;
   flex-wrap: wrap;
+  display: flex;
+  justify-content: center;
   }
 
 .headerLink {
   padding: 6px;
+  padding-left: 32px;
+  padding-right: 32px;
   font-size: 24px;
+}
+
+.headerLink:hover {
+  text-decoration: underline;
+  color: darkblue;
+}
+
+.buttonLink:hover {
+  text-decoration: underline;
+  color: darkblue;
 }
 
 </style>
--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="homepageWrapper">
-    <p>Welcome to Pollokshields!</p>
-    <p>The original, the biggest and best-preserved planned garden suburb in the UK.</p>
-    <p>A Conservation area</p>
-    <img src="https://pollokshieldsheritage.co.uk/img/PH%20web%20home%20image.jpg" alt="Images Of Pollokshields">
-    <div>
-      <p>AGM Reports</p>
-      <p>Latest Planning Applications</p>
-      <p>Lecture Series And AGM
-        <span id="redTextHighlight">Postponed</span> </p>
+  <div id="pageContentWrapper">
+    <div id="pageContentWrapperUpper">
+      <h3>Welcome to Pollokshields!</h3>
+      <p>The original, the biggest and best-preserved planned garden suburb in the UK.</p>
+    </div>
+    <div id="pageContentWrapperLower">
+      <h4>Useful Links</h4>
+      <p class="pretendLink">A Conservation area</p>
+      <p class="pretendLink">AGM Reports</p>
+      <p class="pretendLink">Latest Planning Applications</p>
+      <h4>Notices</h4>
+      <p>Lecture Series And AGM <span id="redTextHighlight">Postponed</span> </p>
       <p>Guided Walk Spring 2020 <span id="redTextHighlight">Cancelled</span> </p>
     </div>
   </div>
+    <div id ="imgWrapper">
+    <img src="https://pollokshieldsheritage.co.uk/img/PH%20web%20home%20image.jpg" alt="Images Of Pollokshields">
+    </div>
+  </div>
 </template>
+
 <script>
 export default {
   name: "home",
@@ -20,16 +28,37 @@ export default {
 </script>
 <style scoped>
 
+h3 {
+  font-family: georgia;
+}
+
+h4 {
+  font-family: georgia;
+  font-size: 22px;
+}
+
 #homepageWrapper {
   text-align: center;
+  display: flex;
+  flex-wrap:
 }
 
 #redTextHighlight {
   color: red;
 }
 
-img {
+#pageContentWrapper {
+  margin-top: 40px;
+}
 
+p {
+  font-family: Garamond;
+  font-size: 18px;
+}
+
+.pretendLink:hover {
+  text-decoration: underline;
+  color: darkblue;
 }
 
 </style>
